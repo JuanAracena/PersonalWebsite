a64d57f96c8771806df986296438fc5738d94185
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2024-08-09 01:06:48 -0400

    added profile page

---
+++ src/components/Profile.js
@@ -0,0 +1,23 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import "./ProfileStyles.css";
+
+
+function Profile() {
+    return (
+        <div id="info">
+            <h1>Juan Aracena</h1>
+            <div id="text">
+                <h2>Software Developer</h2>
+            </div>
+            <img id="portrait" src="photo.jpg" alt=""></img>
+            
+            <div id="icons">
+                <a href="https://github.com/JuanAracena"> <FaGithub size="4em"/></a>
+                <a href="https://www.linkedin.com/in/juan-aracena-santelises/"> <FaLinkedin size="4em"/></a>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Profile;
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function Projects() {
+    return (
+        <div id="list">
+            <h1>Projects</h1>
+        </div>
+    )
+}
+
+export default Projects
