4963d658ca44c6e3950fb822df6da2dc6ff58597
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2024-08-13 19:56:58 -0400

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Profile from "./components/Profile"
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <Profile />
+    <BrowserRouter>
+      <div id="background">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
