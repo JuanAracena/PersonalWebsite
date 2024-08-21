679afa2c47c6f4554305a8f4281b6829d47e9b8c
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2024-08-20 20:49:25 -0400

    Added display button for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,24 @@
-import React from "react"
-
+import React from "react";
+import {FaGithub} from "react-icons/fa";
+import './ProjectsStyles.css'
 
 function Projects() {
+    // const button = document.querySelector("div description")
+    // console.log(button)
+    
     return (
         <div id="list">
             <h1>Projects</h1>
+            <div id="box">
+                <div classname="project-card">
+                    <img classname="proj" src=""></img>
+                    <div classname="description">
+                        <input id="projButton" type="button" value="Online Auction System"></input>
+                        <a href="https://github.com/JuanAracena/OnlineAuctionSystem"><FaGithub size="4em"/></a>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     )
 }
