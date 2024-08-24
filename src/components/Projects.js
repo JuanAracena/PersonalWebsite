1fd5eb2f5380f52e51c1ef9be01b29f6dd072a4d
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2024-08-23 20:49:21 -0400

    Added hidden div

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,32 @@
 import React from "react";
 import {FaGithub} from "react-icons/fa";
 import './ProjectsStyles.css'
+import {useState} from "react"
+
+
+function hiddenDiv(title, descr, github, isVisible, setIsVisible){
+    
+    
+    return (
+        <div id="curtain">
+            <h1>{title}</h1>
+            <input id="closeButton" type="button" value="Close" onClick={() => setIsVisible}></input>
+            <div id="description">
+                <p>{descr}</p>
+            </div>
+            <div id="link">
+                <a href="https://github.com/JuanAracena/OnlineAuctionSystem"><FaGithub size="4em"/></a>
+            </div>
+
+        </div>
+    )
+}
 
 function Projects() {
     // const button = document.querySelector("div description")
     // console.log(button)
+
+    const [isVisible, setIsVisible] = useState("false");
     
     return (
         <div id="list">
@@ -12,9 +34,14 @@ function Projects() {
             <div id="box">
                 <div classname="project-card">
                     <img classname="proj" src=""></img>
-                    <div classname="description">
-                        <input id="projButton" type="button" value="Online Auction System"></input>
-                        <a href="https://github.com/JuanAracena/OnlineAuctionSystem"><FaGithub size="4em"/></a>
+                    <div classname="content">
+                        <input id="projButton" type="button" value="Online Auction System" onClick={() => setIsVisible(!isVisible)}>
+                        {isVisible ? console.log("Show content") : console.log("Hide content")}
+                        </input>
+                        {isVisible && (
+                            hiddenDiv("title", "descr", "github", isVisible, setIsVisible(!isVisible))
+                        )}     
+                            
                     </div>
                 </div>
                 
