ff461a00f7212a3cb5eaa318c9431a98b42f6577
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2024-09-17 01:17:18 -0400

    Fixed text container size

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,9 +9,9 @@ function Profile() {
             <div id="text">
                 <h2>Software Developer</h2>
             </div>
-            {/* <img id="portrait" src="photo.jpg" alt=""></img>
+            <img id="portrait" src="photo.jpg" alt=""></img>
             
-            <div id="icons">
+            {/* <div id="icons">
                 <a href="https://github.com/JuanAracena"> <FaGithub size="4em"/></a>
                 <a href="https://www.linkedin.com/in/juan-aracena-santelises/"> <FaLinkedin size="4em"/></a>
             </div> */}
