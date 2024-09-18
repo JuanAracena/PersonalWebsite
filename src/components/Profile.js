9130b05d2c97fdc8b133f48c2003ced46fd5c769
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2024-09-18 19:54:22 -0400

    Fixed icons and layout when the width is less than 576px

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,15 +9,12 @@ function Profile() {
             <div id="text">
                 <h2>Software Developer</h2>
             </div>
-            <div id="portrait">
-                {/* <img id="img" src="photo.jpg" alt=""></img> */}
-
-            </div>
+            <div id="portrait"></div>
             
-            {/* <div id="icons">
+            <div id="icons">
                 <a href="https://github.com/JuanAracena"> <FaGithub size="4em"/></a>
                 <a href="https://www.linkedin.com/in/juan-aracena-santelises/"> <FaLinkedin size="4em"/></a>
-            </div> */}
+            </div>
             
         </div>
     )
