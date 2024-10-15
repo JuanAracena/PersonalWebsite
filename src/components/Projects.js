a49b6aa9d64f64591418b2ba73cb9464622f1692
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2024-10-15 17:01:24 -0400

    Added new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,15 @@ function Projects() {
     return (
     <div id="list">
         <h1>Projects</h1>
-        <div id="box">
+        <div id="box">  
+            <div className="project-card">
+                <img className="proj" src="urlShortener.jpg" alt=""></img>
+                <div className="content">
+                    <p className="title">URL Shortener</p>
+                    <p className="descr">Website that creates unique URL shorcuts from a given name</p>
+                    <a href="https://github.com/JuanAracena/URLShortener"><FaGithub size="4em"/></a>
+                </div>
+            </div> 
             <div className="project-card">
                 <img className="proj" src="OAS.jpg" alt=""></img>
                 <div className="content">
@@ -16,7 +24,7 @@ function Projects() {
                     <p className="descr">Application that allows online auctioning and bidding</p>
                     <a href="https://github.com/JuanAracena/OnlineAuctionSystem"><FaGithub size="4em"/></a>
                 </div>
-            </div>        
+            </div>          
         </div>
     </div>
         
